5786a953b569dda767542bc5e5d3a22d2de05250
Rafael <rafael.alvarez@hotmail.es>
2020-12-29 14:03:58 +0100

    css module in welcome.js

--- front/src/Components/Welcome/Welcome.js
+++ front/src/Components/Welcome/Welcome.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Login } from '../Login/Login'
-import WelcomeCss from './Welcome.css'
+import WelcomeCss from './Welcome.module.css'
 
 export const Welcome = () => {
 
@@ -9,34 +9,33 @@ export const Welcome = () => {
     const handleClick = (e) =>{
         e.preventDefault();
         e.target.id === "login" && <Login />
-
     }
 
     return (
         <>
-            <div className="mainContainer">
-                <img src="../../background.jpg" alt="Background" className="BG" />
-                <div className="gradientBG"></div>
+            <div className={WelcomeCss.mainContainer}>
+                <img src="../../background.jpg" alt="Background" className={WelcomeCss.BG} />
+                <div className={WelcomeCss.gradientBG}></div>
             </div>
 
-            <div className="btnRegisterContainer"><button type="button" className="registerBtn" >Registrarme</button></div>
+            <div className={WelcomeCss.btnRegisterContainer}><button type="button" className={WelcomeCss.registerBtn} >Registrarme</button></div>
 
-            <div className="comingInSeparator">
-                <hr className="Separator" />
-                <p className="comingIn">Entrar con</p>
-                <hr className="Separator" />
+            <div className={WelcomeCss.comingInSeparator}>
+                <hr className={WelcomeCss.Separator} />
+                <p className={WelcomeCss.comingIn}>Entrar con</p>
+                <hr className={WelcomeCss.Separator} />
             </div>
 
-            <div className="loginSocialBtnContainer">
-                <button type="button" className="loginSocialBtn google" ><img src="../../login-google.png" alt="login google button" className="loginSocialGImg" /></button>
-                <button type="button" className="loginSocialBtn facebook" ><img src="../../login-facebook.png" alt="login facebook button" className="loginSocialFImg" /></button>
+            <div className={WelcomeCss.loginSocialBtnContainer}>
+                <button type="button" className={`${WelcomeCss.loginSocialBtn} ${WelcomeCss.google}`}><img src="../../login-google.png" alt="login google button" className={WelcomeCss.loginSocialGImg} /></button>
+                <button type="button" className={`${WelcomeCss.loginSocialBtn} ${WelcomeCss.facebook}`} ><img src="../../login-facebook.png" alt="login facebook button" className={WelcomeCss.loginSocialFImg} /></button>
             </div>
 
-            <hr className="downSeparator" />
+            <hr className={WelcomeCss.downSeparator} />
             
-            <div className="loginContainer">
+            <div className={WelcomeCss.loginContainer}>
                 <button type="button" id="login" className={WelcomeCss.loginBtn} onClick={handleClick}>Iniciar sesion con email</button>
-                <a className="comeInAsGuest" href="#">Entrar sin iniciar sesión</a>
+                <a className={WelcomeCss.comeInAsGuest} href="#">Entrar sin iniciar sesión</a>
             </div>
 
         </>
